976335480512063b6e336a0fd86df72a4d7a96fd
1464181608 <1464181608@qq.com>
2021-04-12 16:16:17 +0800

    HJYcharts
    终于干完图表了

--- pages/charts/charts.js
+++ pages/charts/charts.js
@@ -1,92 +1,43 @@
 var wxCharts = require('../../utils/wxcharts-min');
-var app = getApp();
 var lineChart = null;
+
+const utilApi=require('../../utils/promiseTest');
+const app = getApp();
+
 Page({
-    data: {
-      amoutList:[],
-      date:'2021-04'
-    },
-    bindDateChange:function(e){
-      console.log('picker发送选择改变，携带值为',e.detail.value)
-      this.setData({
-        date:e.detail.value
-      })
-      this.updateData();
-    },
-    touchHandler: function (e) {
-        //console.log(lineChart.getCurrentDataIndex(e));
-        lineChart.showToolTip(e, {
-            // background: '#7cb5ec',
-            format: function (item, category) {
-                return category + '日' + item.name + ':' + item.data 
-            }
-        });
-    },    
-    createSimulationData: function () {
-        var that=this;
-        //console.log(that.data.amoutList);
-        wx.request({
-          url: 'http://127.0.0.1:8088/WxDemo/lineCanvas',
-          data:{
-            uid:'2',
-            date:that.data.date
-          },
-          headers:{'Content-Type':'application/json'},
-          success:function(res){
-              that.data.amoutList=res.data;
-              console.log(that.data.amoutList);//有数据
-          }
-      })
-        console.log(that.data.amoutList);
-        var categories = [];
-        var data = that.data.amoutList;
-        for (var i = 0; i < 31; i++) {
-            categories.push(i + 1);
-        }
-        return {
-            categories: categories,
-            data: data
-        }
-    },
-    updateData: function () {
-        var simulationData = this.createSimulationData();
-        //console.log(simulationData.data);//有数据
-        var series = [{
-            name: '收入',
-            data: simulationData.data,
-            format: function (val, name) {
-                return val.toFixed(2) + '万';
-            }
-        }];
-        lineChart.updateData({
-            categories: simulationData.categories,
-            series: series
-        });
-    },
-    onLoad: function (e) {
-        var windowWidth = 320;
+  data: {
+    amoutList:[]
+  },
+  //loadMyData函数用于打印myData的值 
+  loadMyData() {
+    console.log('获取到的数据为:' + this.data.amoutList)
+  },
+  createCharts:function(){
+    var that=this;
+    console.log(this.data.amoutList);
+    var windowWidth = 320;
         try {
             var res = wx.getSystemInfoSync();
             windowWidth = res.windowWidth;
         } catch (e) {
             console.error('getSystemInfoSync failed!');
         }
-        var simulationData = this.createSimulationData();
+        //var simulationData = this.createSimulationData();
         lineChart = new wxCharts({
             canvasId: 'lineCanvas',
             type: 'line',
-            categories: simulationData.categories,
+            categories: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
             animation: true,
             // background: '#f5f5f5',
             series: [{
                 name: '收入',
-                data: simulationData.data,
+                data: that.data.amoutList[0],
                 format: function (val, name) {
-                    return val.toFixed(2);
+                  return val.toFixed(2);
                 }
             }, {
                 name: '支出',
-                data: [2, 0, 0, 3, 2, 4, 0, 0, 2, 0,2.5,1.8],
+                data: that.data.amoutList[1],
                 format: function (val, name) {
                     return val.toFixed(2);
                 }
@@ -95,7 +46,7 @@ Page({
                 disableGrid: true
             },
             yAxis: {
-                title: this.data.date+'支出收入详情(元)',
+                title:'2020年5月支出收入详情(元)',
                 format: function (val) {
                     return val.toFixed(2);
                 },
@@ -109,5 +60,27 @@ Page({
                 lineStyle: 'curve'
             }
         });
-    }
+  },
+  // 生命周期函数onload用于监听页面加载 
+  onLoad: function() {
+    utilApi.requestPromise('http://127.0.0.1:8088/WxDemo/lineCanvas?uid=2&date=2020-05') 
+    // 使用.then处理结果 
+    .then(res => { 
+      this.setData({
+        amoutList: res.data
+      })
+      console.log(this.data.amoutList)
+      this.loadMyData()
+      this.createCharts();
+    }) 
+  },
+  touchHandler: function (e) {
+    console.log(lineChart.getCurrentDataIndex(e));
+    lineChart.showToolTip(e, {
+        // background: '#7cb5ec',
+        format: function (item, category) {
+            return category + '日 ' + item.name + ':' + item.data 
+        }
+    });
+},   
 });
